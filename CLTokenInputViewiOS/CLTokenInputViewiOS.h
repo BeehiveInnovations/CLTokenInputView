414474779417dbd961efb1be0d77c43d03417c6f
Nikhil Nigade <nikhil@dezinezync.com>
2022-04-06 17:53:31 +0530

    generate framework
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

---
+++ CLTokenInputViewiOS/CLTokenInputViewiOS.h
@@ -0,0 +1,19 @@
+//
+//  CLTokenInputViewiOS.h
+//  CLTokenInputViewiOS
+//
+//  Created by Nikhil Nigade on 06/04/22.
+//  Copyright © 2022 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLTokenInputViewiOS.
+FOUNDATION_EXPORT double CLTokenInputViewiOSVersionNumber;
+
+//! Project version string for CLTokenInputViewiOS.
+FOUNDATION_EXPORT const unsigned char CLTokenInputViewiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLTokenInputViewiOS/PublicHeader.h>
+
+
